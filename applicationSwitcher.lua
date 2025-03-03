36a50efd80c87b1411826dbbe73c32b89fff3e06
Oliver Taylor <1388093+overvale@users.noreply.github.com>
2025-03-02 21:53:35 -0800

    Initial commit

---
+++ applicationSwitcher.lua
@@ -0,0 +1,146 @@
+-- Application Switcher (macOS 9 style)
+-- "Vibe Coded" by Oliver Taylor
+
+-- Configurable settings
+local hotkeyMods = { "cmd", "option" }
+local hotkeyKey = "."
+local iconSizeMenu = { w = 18, h = 18 }
+local iconSizeSwitcher = { w = 20, h = 20 }
+
+-- Create a menubar item
+local switcher = hs.menubar.new()
+
+-- Helper: Retrieve and sort running apps by title
+local function getSortedRunningApps()
+    local runningApps = hs.application.runningApplications()
+    local appList = {}
+    for _, app in ipairs(runningApps) do
+        if app:kind() == 1 and app:title() and app:title() ~= "" then
+            table.insert(appList, app)
+        end
+    end
+    table.sort(appList, function(a, b)
+        return a:title() < b:title()
+    end)
+    return appList
+end
+
+-- Generate menu items
+local function makeMenu()
+    local menuItems = {}
+    local currentApp = hs.application.frontmostApplication()
+    local runningApps = hs.application.runningApplications()
+
+    -- Determine counts for visible and hidden apps
+    local visibleCount = 0
+    local hasHidden = false
+    for _, app in ipairs(runningApps) do
+        if app:kind() == 1 then
+            if not app:isHidden() and app ~= currentApp then
+                visibleCount = visibleCount + 1
+            elseif app:isHidden() then
+                hasHidden = true
+            end
+        end
+    end
+
+    -- Add hide/show options for the current app
+    if currentApp then
+        local isFinder = currentApp:bundleID() == "com.apple.finder"
+        table.insert(menuItems, {
+            title = "Hide " .. currentApp:title(),
+            fn = isFinder and nil or function() currentApp:hide() end,
+            disabled = isFinder
+        })
+
+        table.insert(menuItems, {
+            title = "Hide Others",
+            fn = function()
+                for _, app in ipairs(runningApps) do
+                    if app ~= currentApp and app:kind() == 1 and not app:isHidden() then
+                        app:hide()
+                    end
+                end
+            end,
+            disabled = visibleCount == 0
+        })
+
+        table.insert(menuItems, {
+            title = "Show All",
+            fn = function()
+                for _, app in ipairs(runningApps) do
+                    if app:kind() == 1 and app:isHidden() then
+                        app:unhide()
+                    end
+                end
+            end,
+            disabled = not hasHidden
+        })
+
+        table.insert(menuItems, { title = "-" }) -- separator
+    end
+
+    -- Use a sorted list of running applications for consistent ordering
+    local sortedApps = getSortedRunningApps()
+
+    -- Add each running app to the menu with its icon
+    for _, app in ipairs(sortedApps) do
+        local title = app:title()
+        -- Gray out hidden apps
+        if app:isHidden() then
+            title = hs.styledtext.new(title, {
+                color = { red = 0.5, green = 0.5, blue = 0.5 }
+            })
+        end
+
+        local menuItem = {
+            title = title,
+            fn = function() app:activate() end
+        }
+
+        if app:bundleID() then
+            local icon = hs.image.imageFromAppBundle(app:bundleID())
+            if icon then
+                menuItem.image = icon:template(false):setSize(iconSizeMenu)
+            end
+        end
+
+        table.insert(menuItems, menuItem)
+    end
+
+    return menuItems
+end
+
+-- Update the menubar icon to the current app's icon
+local function updateIcon()
+    local app = hs.application.frontmostApplication()
+    if app and app:bundleID() then
+        local icon = hs.image.imageFromAppBundle(app:bundleID())
+        if icon then
+            switcher:setIcon(icon:template(false):setSize(iconSizeSwitcher), false)
+        end
+    end
+end
+
+-- Set the menu and initial icon
+switcher:setMenu(makeMenu)
+updateIcon()
+
+-- Watch for app activation events (when an application becomes active)
+local appWatcher = hs.application.watcher.new(function(appName, eventType, app)
+    if eventType == hs.application.watcher.activated then
+        updateIcon()
+    end
+end)
+appWatcher:start()
+
+-- Also watch for window focus changes using hs.window.filter
+local windowFilter = hs.window.filter.new(nil)
+windowFilter:subscribe(hs.window.filter.windowFocused, function()
+    updateIcon()
+end)
+
+-- Bind a keyboard shortcut to show the switcher menu
+hs.hotkey.bind(hotkeyMods, hotkeyKey, function()
+    switcher:popupMenu(switcher:frame())
+end)
---
+++ init.lua
@@ -0,0 +1,378 @@
+-- Oliver Taylor's Hammerspoon Config
+
+
+-- Setup
+-- -----------------------------------------------
+
+function keyUpDown(modifiers, key)
+    hs.eventtap.keyStroke(modifiers, key, 0)
+end
+
+anycomplete = hs.loadSpoon("Anycomplete")
+anycomplete.engine = "duckduckgo"
+anycomplete.bindHotkeys()
+
+require("applicationSwitcher")
+
+-- Functions
+-- -----------------------------------------------
+
+function menuCapitalize()
+    local app = hs.application.frontmostApplication()
+    app:selectMenuItem({ "Edit", "Transformations", "Capitalize" })
+end
+
+function menuUpperCase()
+    local app = hs.application.frontmostApplication()
+    app:selectMenuItem({ "Edit", "Transformations", "Make Upper Case" })
+end
+
+function menuLowerCase()
+    local app = hs.application.frontmostApplication()
+    app:selectMenuItem({ "Edit", "Transformations", "Make Lower Case" })
+end
+
+function newFinderWindow()
+    local finder = hs.appfinder.appFromName("Finder")
+    hs.osascript.applescript('tell application "Finder" to make new Finder window')
+    finder:activate()
+end
+
+function openFolder(folderPath)
+    os.execute('open "' .. folderPath .. '"')
+end
+
+function openApp(appName)
+    os.execute('open -a "' .. appName .. '"')
+end
+
+function runScript(script)
+    os.execute(script)
+end
+
+function backupCloud()
+    hs.osascript.applescript('tell app \"Terminal\" to do script \"backup-cloud\"')
+    hs.application.launchOrFocus("Terminal")
+end
+
+function lastBackupCloud()
+    local output, _, _ = hs.execute("/Users/oliver/src/rsync-backup/last-backup-cloud.sh")
+    return output
+end
+
+function openLastBackup()
+    os.execute('open "$(echo ~/src/rsync-backup/logs/$(ls ~/src/rsync-backup/logs | tail -1))"')
+end
+
+function logbookNew()
+    os.execute("~/src/bin/logg")
+end
+
+function logbookShow()
+    os.execute("open ~/Documents/log/")
+end
+
+function openTODO()
+    os.execute("open ~/Documents/todo.txt")
+end
+
+function insertDate()
+    local dateStr = os.date("%a, %d %B %Y")
+    hs.eventtap.keyStrokes(dateStr)
+end
+
+function receivedThanks()
+    local win = hs.window.focusedWindow()
+    if win then
+        win:focus()
+        -- Wait briefly before sending keystrokes
+        hs.timer.doAfter(0.05, function()
+            hs.eventtap.keyStrokes("Bid received, thank you!")
+        end)
+    end
+end
+
+function xLookupHelper()
+    local win = hs.window.focusedWindow()
+    if win then
+        win:focus()
+        -- Wait briefly before sending keystrokes
+        hs.timer.doAfter(0.05, function()
+            hs.eventtap.keyStrokes("=XLOOKUP([@[Shot Code]],")
+        end)
+    end
+end
+
+function logbook1923New()
+    os.execute("~/src/bin/log1923")
+end
+
+function logbook1923Show()
+    os.execute("open ~/Documents/Work/1923/log/")
+end
+
+function vendorMailer()
+    hs.osascript.applescript(
+        'tell app \"Terminal\" to do script \"cd ~/src/vendor-mailer/ && python3 vendor_mailer.py\"')
+    hs.application.launchOrFocus("Terminal")
+end
+
+function bidPackGen()
+    hs.osascript.applescript(
+        'tell app \"Terminal\" to do script \"cd ~/src/bid-pack-generator/ && ./bid-pack-generator.sh"')
+    hs.application.launchOrFocus("Terminal")
+end
+
+function bidPackMailer()
+    hs.osascript.applescript(
+        'tell app \"Terminal\" to do script \"cd ~/src/vendor-mailer/ && ./bidpak_gen.py\"')
+    hs.application.launchOrFocus("Terminal")
+end
+
+
+function bidFinder()
+    hs.osascript.applescript('tell app \"Terminal\" to do script \"cd ~/src/bid-finder/ && ./bid-finder"')
+    hs.application.launchOrFocus("Terminal")
+end
+
+-- In Pages there is no button or menu item to TOGGLE the sidebar.
+-- Thus this function is a bit of a hack.
+function pagesSidebarToggle()
+    local app = hs.application.frontmostApplication()
+    if app:name() ~= "Pages" then
+        hs.alert.show("Error: Pages is not the active application!")
+        return
+    end
+
+    local menuBar = app:findMenuItem({"View", "Table of Contents"})
+    if menuBar and menuBar["ticked"] then
+        app:selectMenuItem({"View", "Document Only"})
+    else
+        app:selectMenuItem({"View", "Table of Contents"})
+    end
+end
+
+-- Why doesn't ChatGTP have a shortcut for voice input?
+function talk2ChatGPT()
+    hs.execute('shortcuts run "Talk to ChatGPT Voice"')
+end
+
+
+---- Menu Bar ----
+
+function toggleMenubar()
+    hs.applescript([[
+  tell application "System Events"
+    tell dock preferences to set autohide menu bar to not autohide menu bar
+  end tell]])
+end
+
+---- Dark Mode ----
+
+function darkModeStatus()
+    -- return the status of Dark Mode
+    local _, darkModeState = hs.osascript.javascript(
+        'Application("System Events").appearancePreferences.darkMode()'
+    )
+    return darkModeState
+end
+
+function setDarkMode(state)
+    -- Function for setting Dark Mode on/off.
+    -- Argument should be either 'true' or 'false'.
+    return hs.osascript.javascript(
+        string.format(
+            "Application('System Events').appearancePreferences.darkMode.set(%s)", state
+        ))
+end
+
+function toggleDarkMode()
+    -- Toggle Dark Mode status
+    -- Argument should be either 'true' or 'false'.
+    if darkModeStatus() then
+        setDarkMode(false)
+    else
+        setDarkMode(true)
+    end
+end
+
+-- Key Bindings
+-- ----------------------------------------------
+
+-- Accepts only function names
+keyBindings = {
+    { { 'alt', 'cmd' },         'm', toggleMenubar },
+    { { 'ctrl', 'alt', 'cmd' }, 'd', toggleDarkMode },
+    { { 'alt', 'shift' },     'space', talk2ChatGPT },
+}
+
+for i, mapping in ipairs(keyBindings) do
+    local mod = mapping[1]
+    local key = mapping[2]
+    local fn  = mapping[3]
+    hs.hotkey.bind(mod, key, function() fn() end)
+end
+
+
+-- HammerMenu
+-- ----------------------------------------------
+
+hammerMenu = hs.menubar.new()
+
+function hammerMenuItem()
+    local hammerMenuTable = {
+        { title = "􀪏 Build TODO List", fn = function() runScript("~/src/bin/todo") end },
+        { title = "􀞀 Insert Date", fn = insertDate },
+        { title = "-" },
+        { title = "Open Today's Log Entry", fn = logbookNew },
+        { title = "Open Logbook", fn = logbookShow },
+        { title = "-" },
+        { title = "Backup to Cloud", fn = backupCloud },
+        { title = "Latest Backup:", disabled = true },
+        { title = lastBackupCloud(), fn = openLastBackup },
+        { title = "-" },
+        { title = "1923", disabled = true },
+        { title = "1923 Log Entry (Today)", fn = logbook1923New },
+        { title = "1923 Logbook", fn = logbook1923Show },
+        { title = "1923 Tools", disabled = true },
+        { title = "Mail BIDPAK", fn = bidPackMailer },
+        { title = "Vendor Mailer", fn = vendorMailer },
+        { title = "Bid Pack Generator", fn = bidPackGen },
+        { title = "Bid Finder", fn = bidFinder },
+    }
+    return hammerMenuTable
+end
+
+hammerIcon = hs.styledtext.new("􀂢", {
+    font = { size = 17 },
+    baselineOffset = -2,
+})
+
+hammerMenu:setTitle(hammerIcon)
+hammerMenu:setMenu(hammerMenuItem)
+hammerMenu:setTooltip("Hammerspoon Rocks!")
+
+
+-- Transient Keymaps
+-- ----------------------------------------------
+
+screen = hs.screen.mainScreen()
+screenFrame = screen:frame()
+
+transientBanner = {}
+transientBindings = {}
+transientKeys = hs.hotkey.modal.new()
+
+-- When the modal keymap is activated...
+function transientKeys:entered()
+    -- First create a menubar item
+    myKeysMenuItem = hs.menubar.new():setTitle("􀇳 Transient Keymap!")
+    myKeysMenuItem:setTooltip("Press Escape to deactivate.")
+
+    -- Then create the notification banner atop the menubar
+    transientBanner = hs.canvas.new({ x = screenFrame.x, y = 0, w = screenFrame.w, h = 30 })
+    transientBanner:appendElements({
+        type = "rectangle",
+        action = "fill",
+        fillColor = { red = .3, green = 1, blue = .2, alpha = 1 },
+        frame = { x = 0, y = 0, w = "100%", h = "100%" }
+    })
+
+    -- And show it
+    transientBanner:show()
+end
+
+-- When the modal keymap is exited, kill the displayed helpers
+function transientKeys:exited()
+    myKeysMenuItem:delete()
+    transientBanner:delete()
+end
+
+-- Bind keys for activating the transient keymap and cheetsheat
+do
+    local mod = { 'shift', 'cmd' }
+    local key = 'space'
+    hs.hotkey.bind(mod, key, function() transientKeys:enter() end)
+    -- prevent recursion by exiting if the hotkey is repeated
+    transientKeys:bind(mod, key, function() transientKeys:exit() end)
+    -- escape should also work as a way to exit
+    transientKeys:bind('', 'escape', function() transientKeys:exit() end)
+end
+
+-- Accepts strings and function names
+-- Strings are assumed to be Application names
+transientBindings = {
+    { {},          'm', 'Mail' },
+    { { 'shift' }, 'm', 'Messages' },
+    { {},          'c', 'Calendar' },
+    { {},          's', 'Safari' },
+    { {},          'a', 'Music' },
+    { {},          't', 'Terminal' },
+    { {},          'f', newFinderWindow },
+    { {},          'n', 'Notes' },
+    { {},          'v', 'Visual Studio Code' },
+    { {},          'e', 'BBEdit' },
+    -- Work
+    { { 'cmd' },   'x', xLookupHelper },
+    { { 'cmd' },   'r', receivedThanks },
+}
+
+for i, mapping in ipairs(transientBindings) do
+    local mod = mapping[1]
+    local key = mapping[2]
+    local fn  = mapping[3]
+    transientKeys:bind(mod, key, function()
+        if (type(fn) == 'string') then
+            hs.application.launchOrFocus(fn)
+        else
+            fn()
+        end
+        transientKeys:exit()
+    end)
+end
+
+
+-- App-Specific Keymaps
+-- ----------------------------------------------
+-- This creates keymaps for specific apps, and creates an application watcher
+-- that activates and deactivates the mappings when the associated app
+-- activates.
+
+-- Create maps you'd like to turn on and off
+-- There can be more than one
+readlineModeMap = hs.hotkey.modal.new()
+pagesModeMap = hs.hotkey.modal.new()
+
+-- Readline
+readlineModeMap:bind({ 'alt' }, 'l', function() menuLowerCase() end)
+readlineModeMap:bind({ 'alt' }, 'c', function() menuCapitalize() end)
+readlineModeMap:bind({ 'alt' }, 'u', function() menuUpperCase() end)
+readlineModeMap:bind({ 'alt' }, 'b', function() keyUpDown({ 'alt' }, 'left') end)
+readlineModeMap:bind({ 'alt' }, 'f', function() keyUpDown({ 'alt' }, 'right') end)
+readlineModeMap:bind({ 'alt' }, 'd', function() keyUpDown({ 'alt' }, 'forwarddelete') end)
+
+-- Pages
+pagesModeMap:bind({ 'cmd', 'alt' }, 's', function() pagesSidebarToggle() end)
+
+-- App Activation Watcher
+function appActivation(appName, eventType, appObject)
+    if (eventType == hs.application.watcher.activated) then
+        -- Readline Mode Map
+        if appName == "Terminal" or appName == "Microsoft Excel" then
+            readlineModeMap:exit()
+        else
+            readlineModeMap:enter()
+        end
+        -- Pages Mode Map
+        if appName == "Pages" then
+            pagesModeMap:enter()
+        else
+            pagesModeMap:exit()
+        end
+    end
+end
+
+appActivationWatcher = hs.application.watcher.new(appActivation)
+appActivationWatcher:start()
+
+-- END HAMMERSPOON CONFIG --
